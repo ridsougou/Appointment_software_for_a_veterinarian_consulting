5bcd8e57c4f1178ceeb834ac6f62aab50ef069d8
Redouane ID SOUGOU <122360781+ridsougou@users.noreply.github.com>
2023-04-05 09:40:08 +0200

    Add source code

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "fonction.h"
+#define MAX 10
+
+
+
+
+int main()
+{
+
+        MENU_PRINCIPAL();
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
